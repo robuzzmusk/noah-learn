cf423a4f6f355c57cc3c7069ec4dde35bbec85db
robuzzmusk <robuzzmusk@gmail.com>
2024-09-30 19:19:45 +0000

    fixing indentation

--- randomfruit.py
+++ randomfruit.py
@@ -1,15 +1,18 @@
 from random import choices
 
+
 def fruit():
     fruits = ["apple", "cherry", "strawberry"]
     return choices(fruits)[0]  # Fixed index from 'o' to '0'
 
+
 def meal(beverage):
     my_fruit = fruit()
-    print(f"Your fruit is {my_fruit} and your beverage is {beverage}")  # Fixed the missing quote
+    print(
+        f"Your fruit is {my_fruit} and your beverage is {beverage}"
+    )  # Fixed the missing quote
     if my_fruit == "cherry":
         complete_meal = f"Your meal is a {my_fruit} and {beverage}"
         return complete_meal
     alternate_meal = f"Your meal is steak and {beverage}"
     return alternate_meal
-
